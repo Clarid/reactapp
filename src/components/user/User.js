de42b5d2f643b3218d1c13144a17739176909b1b
Artur Yanovich <yanovichartur89@mail.ru>
2019-12-05 18:54:02 +0300

    Styles and events

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,13 +1,24 @@
 import React from 'react'
-import './User.css'
+import classes from './User.css'
 
 class User extends React.Component {
     render() {
+        const inputClasses = [];
+        if (!this.props.name.length) {
+            inputClasses.push(classes.red);
+        } else {
+            inputClasses.push(classes.green);
+        }
+
+        if (this.props.name.length > 4) {
+            inputClasses.push(classes.italic);
+        }
+
         return (
-            <div className="User">
+            <div className={classes.User}>
                 <div>{this.props.name}</div>
                 <div>Year: {this.props.age}</div>
-                <input onChange={this.props.onChangeName} type="text" value={this.props.name}/>
+                <input className={inputClasses.join(' ')} onChange={this.props.onChangeName} type="text" value={this.props.name}/>
                 <button onClick={this.props.onChangeTitle}>Change Title</button>
                 <button onClick={this.props.onDelete}>Delete</button>
             </div>
